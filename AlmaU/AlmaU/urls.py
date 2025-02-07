a2aac602bb6c692bd993f0ed3f0ae807b448ef51
Beka <ubniyev.b@gmail.com>
2025-02-07 20:49:44 +0000

    cr-3

--- AlmaU/AlmaU/urls.py
+++ AlmaU/AlmaU/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('students.urls')),
     path('api/', include('subjects.urls')),
+    path('todos/', include('todos.urls')),
 ]
